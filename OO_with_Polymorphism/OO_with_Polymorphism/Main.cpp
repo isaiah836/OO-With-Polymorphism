efbec7f89c1c16aef77f5d1a2e51e43ed8a8c9ba
isaiah836 <isasanch@uat.edu>
2018-09-11 12:21:27 -0700

    added more code

---
+++ OO_with_Polymorphism/OO_with_Polymorphism/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+#include "Shapes.h"
+using namespace std;
+
+void main()
+{
+	
+}
--- OO_with_Polymorphism/OO_with_Polymorphism/Shapes.h
+++ OO_with_Polymorphism/OO_with_Polymorphism/Shapes.h
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef SHAPES_H
+#define SHAPES_H
 
 #include <string>
 using namespace std;
@@ -7,5 +9,26 @@ class Shape
 {
 public:
 	Shape();
+	Shape(string N, string C);
 
+	string GetName();
+	void SetName(string);
+	string GetColor();
+	void SetColor(string);
+	void Display();
+protected:
+	string name;
+	string color;
+private:
 };
+
+class Rectangle : public Shape
+{
+public:
+	Circle();
+	Circle(string N, string C, int R);
+	int GetRadius();
+	void SetRadius();
+	void Display
+}
+#endif

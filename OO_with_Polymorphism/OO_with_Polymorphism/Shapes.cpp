3ece259c5b2093627cbd61a2f7b62ce043295dc5
isaiah836 <isasanch@uat.edu>
2018-09-16 22:48:44 -0700

    NOTDONE

--- OO_with_Polymorphism/OO_with_Polymorphism/Shapes.cpp
+++ OO_with_Polymorphism/OO_with_Polymorphism/Shapes.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <iomanip>
 #include "Shapes.h"
+using namespace std;
+
+Shape::Shape() {}
+Shape::~Shape() { cout << "Shape destructor called."; }
+
+Shape::Shape(string N, string C)
+{
+	name = N;
+	color = C;
+}
+
+string Shape::GetName() { return name; }
+void Shape::SetName(string N) { name = N; }
